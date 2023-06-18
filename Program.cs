460a76084cc380a00819c327701ed0a26a8b2545
Madhur82954 <itzmadhurverma@gmail.com>
2023-06-18 19:27:32 +0530

    [Madhur] - UC13-ReadOrWriteFile Added

--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace AddressBookSystem
 {
@@ -10,14 +11,16 @@ namespace AddressBookSystem
             Console.Write("Enter number of Address Book you want : ");
             int numBook = Convert.ToInt32(Console.ReadLine());
             int numberBook = 0;
-            Console.WriteLine("Enter name of address book");
-            string book = Console.ReadLine();
+            List<string> BookNames = new List<string>();
             while (numberBook < numBook)
             {
+                Console.WriteLine("Enter name of address book");
+                string book = Console.ReadLine();
+                BookNames.Add(book);
                 AddressBook address = new AddressBook();
                 while (true)
                 {
-                    Console.WriteLine("Enter Option \n1) Add Contact \n2) Display Contact \n3) Edit Contact \n4) Delete Contact");
+                    Console.WriteLine("Enter Option \n1) Add Contact \n2) Display Contact \n3) Edit Contact \n4) Delete Contact \n5) Save And Exit");
                     int option = Convert.ToInt32(Console.ReadLine());
                     switch (option)
                     {
@@ -34,28 +37,73 @@ namespace AddressBookSystem
                             address.DeleteContact();
                             break;
                         case 5:
+                            SaveDataToFile(AddressBook.con, book);
+                            Console.WriteLine("Data saved to file successfully.");
+                            ReadDataFromFile(); 
+                            return;
+                        case 6:
                             AddressBook.SearchCity();
                             break;
-                        case 6:
+                        case 7:
                             AddressBook.SearchState();
                             break;
-                        case 7:
+                        case 8:
                             address.ViewByCityOrStateName();
                             break;
-                        case 8:
+                        case 9:
                             address.CountByCityOrStateName();
                             break;
-                        case 9:
+                        case 10:
                             address.SortByName(AddressBook.con);
                             break;
-                        case 10:
+                        case 11:
                             address.SortByChoice(AddressBook.con);
                             break;
                     }
+                    
+                }
+                AddressBook.AddTo(book);
+                numberBook++;
+            }
+        }
+        static void SaveDataToFile(List<Contact> addressBook, string bookName)
+        {
+            string path = @"C:\Users\DELL\source\repos\AddressBookSystem\WriteAddressBook.txt";
+
+            using (StreamWriter writer = new StreamWriter(path, true))
+            {
+                foreach (var contact in addressBook)
+                {
+                    writer.WriteLine("Book Name - " + bookName);
+                    writer.WriteLine("FirstName - " + contact.Firstname);
+                    writer.WriteLine("LastName - " + contact.Lastname);
+                    writer.WriteLine("Address - " + contact.Address);
+                    writer.WriteLine("City - " + contact.City);
+                    writer.WriteLine("State - " + contact.State);
+                    writer.WriteLine("Zip - " + contact.Zip);
+                    writer.WriteLine("PhoneNumber - " + contact.PhoneNumber);
+                    writer.WriteLine("E-mail - " + contact.Email);
+                    writer.WriteLine();
                 }
             }
-            AddressBook.AddTo(book);
-            numberBook++;
+        }
+        static void ReadDataFromFile()
+        {
+            string path = @"C:\Users\DELL\source\repos\AddressBookSystem\WriteAddressBook.txt";
+            if (File.Exists(path))
+            {
+                string[] lines = File.ReadAllLines(path);
+
+                Console.WriteLine("Data read from file:");
+                foreach (string line in lines)
+                {
+                    Console.WriteLine(line);
+                }
+            }
+            else
+            {
+                Console.WriteLine("File not found.");
+            }
         }
     }
 }
